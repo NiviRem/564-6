ff1c6a66068ae0ab1c5784d79235340f4018f241
Rago200 <rsenthilkuma@wisc.edu>
2023-12-13 13:08:41 -0600

    Done with delete.C

--- delete.C
+++ delete.C
@@ -16,11 +16,43 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-// part 6
-return OK;
+	Status status = OK;
+	HeapFileScan *scanner = new HeapFileScan(relation, status);
+	if(status != OK) return status;
 
+    if (status != OK) return status;
 
+	if(attrName == ""){
+		status = scanner->startScan(0, 0, type, NULL, op);
+	}
+	else{
+		AttrDesc attrDesc;
+		status = attrCat->getInfo(relation, attrName, attrDesc);
+		if(type == STRING){
+			status = scanner->startScan(attrDesc.attrOffset, attrDesc.attrLen, type, attrValue, op);
+		}
+		else if(type == INTEGER){
+			int intVal = atoi(attrValue);
+			status = scanner->startScan(attrDesc.attrOffset, attrDesc.attrLen, type, (char *)&intVal, op);
+		}
+		else if(type == FLOAT){
+			float floatVal = atof(attrValue);
+			status = scanner->startScan(attrDesc.attrOffset, attrDesc.attrLen, type, (char *)&floatVal, op);
+		}
+	}
 
+	if (status != OK) return status;
+
+	RID rid;
+	while ((status= scanner->scanNext(rid)) != FILEEOF)
+	{
+		status = scanner->deleteRecord();
+		if (status != OK) return status;
+	}
+	scanner->endScan();
+	delete scanner;
+
+	return OK;
 }
 
 
