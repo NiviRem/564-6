c3f41ca4acdf0abae31c6770018c9c19bd6c7080
Rago200 <rsenthilkuma@wisc.edu>
2023-12-14 16:12:07 -0600

    made test script

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+make
+tcsh qutest 1 > qu1.txt
+tcsh qutest 2 > qu2.txt
+tcsh qutest 3 > qu3.txt
+tcsh qutest 4 > qu4.txt
+tcsh qutest 5 > qu5.txt
+tcsh qutest 6 > qu6.txt
+tcsh qutest 7 > qu7.txt
+tcsh qutest 8 > qu8.txt
+tcsh qutest 9 > qu9.txt
+tcsh qutest 10 > qu10.txt
+tcsh qutest 11 > qu11.txt
+tcsh qutest 12 > qu12.txt
+make clean
